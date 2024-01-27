c8a43ce364e5447b6d955700f648b1f4d0f8fbe7
Venkatesh Chapalamadugu <96712283+venkateshchapalamadugu27@users.noreply.github.com>
2024-01-27 21:21:31 +0530

    Time: 1 ms (67.73%), Space: 41.1 MB (8.44%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.java
@@ -0,0 +1,12 @@
+
+class Solution {
+    public boolean isUgly(int n) {
+        if(n<=0) return false;
+        for(int factor:new int[]{2,3,5}){
+            while(n%factor==0){
+                n=n/factor;
+            }
+        }
+        return n==1;
+    }
+}
