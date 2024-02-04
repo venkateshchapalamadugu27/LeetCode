64b033c55acdd3ab5e7e8854adfd78c4ef57713d
Venkatesh Chapalamadugu <96712283+venkateshchapalamadugu27@users.noreply.github.com>
2024-02-04 21:28:09 +0530

    Time: 523 ms (5.55%), Space: 11.7 MB (83.66%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    import math
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        for x in range(0,1000):
+            if n==math.pow(2,x):
+                return True
+                
+            else:
+                print('false')
+                
