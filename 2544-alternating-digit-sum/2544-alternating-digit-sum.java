1adf16423adf0218c1213de4ff392bce09bc935a
Venkatesh Chapalamadugu <96712283+venkateshchapalamadugu27@users.noreply.github.com>
2024-02-09 19:36:43 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (45.50%) - LeetHub

--- 2544-alternating-digit-sum/2544-alternating-digit-sum.java
+++ 2544-alternating-digit-sum/2544-alternating-digit-sum.java
@@ -1,25 +1,42 @@
+// class Solution {
+//     public int alternateDigitSum(int n) {
+//         int odd=0,even=0,N=0;
+//         while(n>0){
+//             int rem=n%10;
+//             N=N*10+rem;
+//             n=n/10;
+//         }
+//         ArrayList<Integer> list=new ArrayList<>();
+//         while(N>0){
+//             int rem=N%10;
+//             list.add(rem);
+//             N=N/10;
+//         }
+//         for(int i=0;i<list.size();i++){
+//             if(i%2==0) {
+//                even+=list.get(i);
+//             }
+//             else{
+//                 odd+=list.get(i);
+//             }
+//         }
+//         return even-odd;
+//     }
+// }
+
 class Solution {
     public int alternateDigitSum(int n) {
-        int odd=0,even=0,N=0;
-        while(n>0){
-            int rem=n%10;
-            N=N*10+rem;
-            n=n/10;
-        }
-        ArrayList<Integer> list=new ArrayList<>();
-        while(N>0){
-            int rem=N%10;
-            list.add(rem);
-            N=N/10;
-        }
-        for(int i=0;i<list.size();i++){
-            if(i%2==0) {
-               even+=list.get(i);
+        int odd=0,even=0;
+        String  s= Integer.toString(n);
+        for(int i=0;i<s.length();i++){
+            if(i%2==0){
+                even+=Integer.parseInt(String.valueOf(s.charAt(i)));
             }
             else{
-                odd+=list.get(i);
+                odd+=Integer.parseInt(String.valueOf(s.charAt(i)));
             }
         }
+        
         return even-odd;
     }
 }
