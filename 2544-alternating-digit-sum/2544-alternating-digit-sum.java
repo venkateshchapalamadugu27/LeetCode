cd898a4379f7fb82ef59e78f7d64979f402426f7
Venkatesh Chapalamadugu <96712283+venkateshchapalamadugu27@users.noreply.github.com>
2024-02-09 19:13:29 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (67.83%) - LeetHub

---
+++ 2544-alternating-digit-sum/2544-alternating-digit-sum.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int alternateDigitSum(int n) {
+        int odd=0,even=0,N=0;
+        while(n>0){
+            int rem=n%10;
+            N=N*10+rem;
+            n=n/10;
+        }
+        ArrayList<Integer> list=new ArrayList<>();
+        while(N>0){
+            int rem=N%10;
+            list.add(rem);
+            N=N/10;
+        }
+        for(int i=0;i<list.size();i++){
+            if(i%2==0) {
+               even+=list.get(i);
+            }
+            else{
+                odd+=list.get(i);
+            }
+        }
+        return even-odd;
+    }
+}
