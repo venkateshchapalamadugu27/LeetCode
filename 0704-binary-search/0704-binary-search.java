4c5a85292ffe20ced3dfba84883f6a1c2ab12232
Venkatesh Chapalamadugu <96712283+venkateshchapalamadugu27@users.noreply.github.com>
2024-02-14 12:51:59 +0530

    Time: 0 ms (100.00%), Space: 45.7 MB (49.68%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int search(int[] arr, int target) {
+        int start=0;
+        int end=arr.length-1;
+        // Arrays.sort(arr);
+        while(start<=end){
+           int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
