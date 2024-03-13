075b4c17194e5bf27614ab5dda567e75dea948a2
Venkatesh Chapalamadugu <96712283+venkateshchapalamadugu27@users.noreply.github.com>
2024-03-13 10:04:43 +0530

    Time: 3 ms (99.86%), Space: 49.3 MB (32.47%) - LeetHub

---
+++ 0470-implement-rand10-using-rand7/0470-implement-rand10-using-rand7.java
@@ -0,0 +1,15 @@
+/**
+ * The rand7() API is already defined in the parent class SolBase.
+ * public int rand7();
+ * @return a random integer in the range 1 to 7
+ */
+/* We need to return a random number from 
+*/
+class Solution extends SolBase {
+    int x=1;
+    public int rand10() {
+        if(x>=11) 
+            x=1;
+        return x++;
+    }
+}
