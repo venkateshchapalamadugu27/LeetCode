8d620b985ecc17213add37d4811515076d2637ad
Venkatesh Chapalamadugu <96712283+venkateshchapalamadugu27@users.noreply.github.com>
2024-03-13 10:08:41 +0530

    Time: 4 ms (97.96%), Space: 49.7 MB (5.71%) - LeetHub

--- 0470-implement-rand10-using-rand7/0470-implement-rand10-using-rand7.java
+++ 0470-implement-rand10-using-rand7/0470-implement-rand10-using-rand7.java
@@ -3,7 +3,8 @@
  * public int rand7();
  * @return a random integer in the range 1 to 7
  */
-/* We need to return a random number from 
+/* We need to return a random number from 1 to 10 based on the condition.So, I initially initialize x to 1.
+   If it exceeds 10,I put a condition resets to 1 . Return x++ value.
 */
 class Solution extends SolBase {
     int x=1;
