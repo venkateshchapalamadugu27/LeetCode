174b83b4251161159da9602f021bda3ee3abcc74
Venkatesh Chapalamadugu <96712283+venkateshchapalamadugu27@users.noreply.github.com>
2024-03-14 10:37:17 +0530

    Time: 1635 ms (9.50%), Space: 47.4 MB (73.36%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -1,3 +1,7 @@
+/*First check the individual numbers in the nums that sum up to goal or not.
+Then iterate through elements and check cummulative sum from index 0 to 1,index o to 2 and so....on.
+If it exceeds goal break inner loop and increase i by 1 and check index 1 to 2 and index 1 to 3 and so...on.
+If sum==goal icrease count by 1.At last return count value.  */
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
         int count=0;
