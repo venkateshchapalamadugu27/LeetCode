22800ad23f91d9998b4c99dee03a12af1269bce9
Venkatesh Chapalamadugu <96712283+venkateshchapalamadugu27@users.noreply.github.com>
2024-03-21 11:38:04 +0530

    Time: 1 ms (98.65%), Space: 40.8 MB (98.55%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack=new Stack<>();
+        for(char c:s.toCharArray()){
+            if(c=='(' || c=='{' || c=='['){
+                stack.push(c);
+            }
+            else{
+                if(c==')'){
+                    if(stack.isEmpty() || stack.pop()!='('){
+                         return false;
+                    }
+                }
+                if(c=='}'){
+                    if(stack.isEmpty() || stack.pop()!='{'){
+                        return false;
+                   }
+                }
+                
+                if(c==']'){
+                    if(stack.isEmpty() || stack.pop()!='['){
+                            return false;
+                       }
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
