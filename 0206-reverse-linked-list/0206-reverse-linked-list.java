2dbb131c89b2e4ad23914ce2fee51f697ff17ed1
Venkatesh Chapalamadugu <96712283+venkateshchapalamadugu27@users.noreply.github.com>
2024-03-21 09:52:09 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (79.48%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -11,23 +11,42 @@
 class Solution {
     public ListNode reverseList(ListNode head) {
 
-       ArrayList<Integer> arr=new ArrayList<>();
-       ListNode temp=head;
-       while(temp!=null){
-           arr.add(temp.val);
-           temp=temp.next;
-       }
-       Collections.reverse(arr);
-       System.out.println(arr);
+    //    ArrayList<Integer> arr=new ArrayList<>();
+    //    ListNode temp=head;
+    //    while(temp!=null){
+    //        arr.add(temp.val);
+    //        temp=temp.next;
+    //    }
+    //    Collections.reverse(arr);
+    //    System.out.println(arr);
    
-        ListNode dummy = new ListNode();
-        ListNode current = dummy;
+    //     ListNode dummy = new ListNode();
+    //     ListNode current = dummy;
 
-        for (int i = 0; i < arr.size(); i++) {
-            current.next = new ListNode(arr.get(i));
-            current = current.next;
+    //     for (int i = 0; i < arr.size(); i++) {
+    //         current.next = new ListNode(arr.get(i));
+    //         current = current.next;
+    //     }
+    //     return dummy.next;
+
+    if(head==null){
+        return head;
+    }
+       ListNode prev = null;
+       ListNode present = head;
+       ListNode Next = present.next;
+
+        while (present != null) {
+            // Next = present.next;
+            present.next = prev;
+            prev = present;
+            present = Next;
+            if (Next != null) {
+                Next = Next.next;
+            }
         }
-        return dummy.next;
+
+        return prev;
     }
     
 }
