8d313e7204cb25d9bd9ad255e5d60273fe517b60
Venkatesh Chapalamadugu <96712283+venkateshchapalamadugu27@users.noreply.github.com>
2024-05-12 18:50:16 +0530

    Time: 1 ms (100.00%), Space: 42.7 MB (100.00%) - LeetHub

--- 3146-permutation-difference-between-two-strings/3146-permutation-difference-between-two-strings.java
+++ 3146-permutation-difference-between-two-strings/3146-permutation-difference-between-two-strings.java
@@ -3,21 +3,33 @@ class Solution {
         int[] arr1=new int[26];
         int[] arr2=new int[26];
         
-        for(int i=0;i<s.length();i++){
-            int index=s.charAt(i)-'a';
-            arr1[index]=i;
-        }
+//         for(int i=0;i<s.length();i++){
+//             int index=s.charAt(i)-'a';
+//             arr1[index]=i;
+//         }
 
-        for(int j=0;j<t.length();j++){
-            int index=t.charAt(j)-'a';
-            arr2[index]=j;
-        }
-
+//         for(int j=0;j<t.length();j++){
+//             int index=t.charAt(j)-'a';
+//             arr2[index]=j;
+//         }
+        
+        arr1=helper(s);
+        arr2=helper(t);
+        
         int res=0;
         for(int i=0;i<26;i++){
             res+=Math.abs(arr1[i]-arr2[i]);
         }
 
         return res;
+    }
+    
+    private int[] helper(String s){
+        int[] array=new int[26];
+        for(int i=0;i<s.length();i++){
+            int index=s.charAt(i)-'a';
+            array[index]=i;
+        }
+        return array;
     }
 }
