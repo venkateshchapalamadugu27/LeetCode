18002bb7bda835207791f9e7bedf31d5348fe4f2
Venkatesh Chapalamadugu <96712283+venkateshchapalamadugu27@users.noreply.github.com>
2024-05-21 06:34:03 +0530

    Time: 1 ms (57.66%), Space: 42.8 MB (16.33%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] arr) {
+        Arrays.sort(arr);
+        List<List<Integer>> outer=new ArrayList<>();
+        outer.add(new ArrayList<>());
+        int start=0;
+        int end=0;
+        for(int i=0;i< arr.length;i++){
+            start=0;
+            if(i>0 && arr[i]==arr[i-1]){
+                start=end+1;
+            }
+            end=outer.size()-1;
+            int n=outer.size();
+            for(int j=start;j<n;j++){
+                List<Integer> internal=new ArrayList<>(outer.get(j));
+                internal.add(arr[i]);
+                outer.add(internal);
+           }
+        }
+        return  outer;
+    }
+}
