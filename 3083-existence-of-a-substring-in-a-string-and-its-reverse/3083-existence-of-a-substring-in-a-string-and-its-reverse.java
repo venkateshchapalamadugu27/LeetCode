f3f81a942d4618964a994c411c9c15e56f5217bc
Venkatesh Chapalamadugu <96712283+venkateshchapalamadugu27@users.noreply.github.com>
2024-07-22 21:32:43 +0530

    Time: 1 ms (100.00%), Space: 42.3 MB (64.53%) - LeetHub

--- 3083-existence-of-a-substring-in-a-string-and-its-reverse/3083-existence-of-a-substring-in-a-string-and-its-reverse.java
+++ 3083-existence-of-a-substring-in-a-string-and-its-reverse/3083-existence-of-a-substring-in-a-string-and-its-reverse.java
@@ -2,9 +2,9 @@ class Solution {
     public boolean isSubstringPresent(String s) {
         for(int i=0;i<s.length()-1;i++){
             // String sub=s.substring(i,i+2);
-            String sb=new StringBuilder(s.substring(i,i+2)).reverse().toString();
+            // String sb=new StringBuilder(s.substring(i,i+2)).reverse().toString();
             
-            if(s.contains(sb)) return true;
+            if(s.contains(new StringBuilder(s.substring(i,i+2)).reverse().toString())) return true;
         }
         return false;
     }
