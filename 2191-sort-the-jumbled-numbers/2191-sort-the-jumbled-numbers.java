16cdd5c62ff34800f9b632ba871c948e7606a226
Venkatesh Chapalamadugu <96712283+venkateshchapalamadugu27@users.noreply.github.com>
2024-07-24 19:04:36 +0530

    Time: 142 ms (69.61%), Space: 55.2 MB (93.14%) - LeetHub

---
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int[] sortJumbled(int[] mapping, int[] nums) {
+        int[] corrected=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            corrected[i]=helper(mapping,nums[i]);
+        }
+        Integer[] indices = new Integer[corrected.length];
+        for (int i = 0; i < corrected.length; i++) {
+            indices[i] = i;
+        }
+
+        Arrays.sort(indices, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer i1, Integer i2) {
+                return Integer.compare(corrected[i1], corrected[i2]);
+            }
+        });
+
+        int[] sortedNums = new int[nums.length];
+        for (int i = 0; i < indices.length; i++) {
+            sortedNums[i] = nums[indices[i]];
+        }
+
+        return sortedNums;
+    }
+
+    private static int helper(int[] mapping, int num){
+       int res=0;
+       String s=Integer.toString(num);
+       for(int i=0;i<s.length();i++){
+          res=res*10+mapping[Integer.parseInt(String.valueOf(s.charAt(i)))];
+       }
+       return res;
+    }
+}
+
+
+/*
+
+[991,338,38]
+
+[8,9,4,0,2,1,3,5,7,6]
+[0,1,2,3,4,5,6,7,8,9]
+
+[669,007,07]
+
+*/
