679a403893875c1b6cc9a5b6c89a8f024e80784e
Venkatesh Chapalamadugu <96712283+venkateshchapalamadugu27@users.noreply.github.com>
2024-08-20 19:31:33 +0530

    Time: 142 ms (64.36%), Space: 55.3 MB (83.18%) - LeetHub

--- 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.java
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.java
@@ -1,38 +1,40 @@
-class Solution {
-    public int[] sortJumbled(int[] mapping, int[] nums) {
+class Solution                                                             {
+    public int[] sortJumbled(int[] mapping, int[] nums)                    {
         int[] corrected=new int[nums.length];
-        for(int i=0;i<nums.length;i++){
+        for(int i=0;i<nums.length;i++)                                     {
             corrected[i]=helper(mapping,nums[i]);
-        }
+                                                                           }
         Integer[] indices = new Integer[corrected.length];
-        for (int i = 0; i < corrected.length; i++) {
+        for (int i = 0; i < corrected.length; i++)                         {
             indices[i] = i;
-        }
+                                                                           }
 
-        Arrays.sort(indices, new Comparator<Integer>() {
+        Arrays.sort(indices, new Comparator<Integer>()                     {
             @Override
-            public int compare(Integer i1, Integer i2) {
+            public int compare(Integer i1, Integer i2)                     {
                 return Integer.compare(corrected[i1], corrected[i2]);
-            }
-        });
+                                                                           }
+                                                                           }
+                                                                           )
+                                                                           ;
 
         int[] sortedNums = new int[nums.length];
-        for (int i = 0; i < indices.length; i++) {
+        for (int i = 0; i < indices.length; i++)                           {
             sortedNums[i] = nums[indices[i]];
-        }
+                                                                           }
 
         return sortedNums;
-    }
+                                                                           }
 
-    private static int helper(int[] mapping, int num){
+    private static int helper(int[] mapping, int num)                      {
        int res=0;
        String s=Integer.toString(num);
-       for(int i=0;i<s.length();i++){
-          res=res*10+mapping[Integer.parseInt(String.valueOf(s.charAt(i)))];
-       }
+       for(int i=0;i<s.length();i++)                                       {
+        res=res*10+mapping[Integer.parseInt(String.valueOf(s.charAt(i)))];
+                                                                           }
        return res;
-    }
-}
+                                                                           }
+                                                                           }
 
 
 /*
