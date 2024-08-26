233011aea08700bb6c4fa6322f2007ed946865d9
Venkatesh Chapalamadugu <96712283+venkateshchapalamadugu27@users.noreply.github.com>
2024-08-26 19:15:16 +0530

    Time: 8 ms (6.88%), Space: 44.8 MB (18.49%) - LeetHub

---
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+import java.util.*;
+import java.math.BigInteger;
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode temp1=l1,temp2=l2;
+        StringBuilder s1=new StringBuilder();
+        StringBuilder s2=new StringBuilder();
+        while(temp1!=null){
+            s1.append(temp1.val);
+            temp1=temp1.next;
+        }
+        while(temp2!=null){
+            s2.append(temp2.val);
+            temp2=temp2.next;
+        }
+        
+        BigInteger b1=new BigInteger(s1.toString());  
+        BigInteger b2=new BigInteger(s2.toString());
+        BigInteger b3=b1.add(b2);
+        String s3=b3.toString();
+
+        ListNode dummy = new ListNode();
+        ListNode current = dummy;
+        for(int i=0;i<s3.length();i++) {
+            current.next = new ListNode(Integer.parseInt(String.valueOf(s3.charAt(i))));
+            current = current.next;
+        }
+
+        return dummy.next;
+    }
+}
